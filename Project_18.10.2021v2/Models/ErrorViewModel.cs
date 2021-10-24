31af2941e79770d2680d46987ed5b636b0d6789d
D1MOND12 <73625441+D1MOND12@users.noreply.github.com>
2021-10-24 11:41:57 +1000

    Add project files.

---
+++ Project_18.10.2021v2/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace Project_18._10._2021v2.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
